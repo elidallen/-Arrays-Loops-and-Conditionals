6de603c8a473d3822618960951fd8b42415bd8a0
elidallen <mitchellallen2@yahoo.com>
2023-10-20 15:43:58 -0400

    Get Even answered

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ for (let i = 0; i <= 200; i += 2) {
     console.log(i);
 }
 // Commit message: "Get Even answered"
-
+// Commit message: "Get Even answered"
 
 ////////////////////////////////
 // Fizz Buzz
